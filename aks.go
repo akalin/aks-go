b7807a1b27f50703041a4dfa08f658b070918535
Frederick Akalin <akalin@gmail.com>
2013-03-19 00:28:45 -0700

    Implement getFirstAKSWitness in aks utility

--- aks.go
+++ aks.go
@@ -21,17 +21,27 @@ func isAKSWitness(n, r, a *big.Int) bool {
 	return isWitness
 }
 
-func main() {
-	n := big.NewInt(175507)
-	r := big.NewInt(337)
-	a := big.NewInt(2)
-	fmt.Printf("n = %v, r = %v, a = %v\n", n, r, a)
-	isWitness := isAKSWitness(n, r, a)
-	var isWitnessStr string
-	if isWitness {
-		isWitnessStr = "is"
-	} else {
-		isWitnessStr = "is not"
+// Returns the first AKS witness of n with the parameters r and M, or
+// nil if there isn't one.
+func getFirstAKSWitness(n, r, M *big.Int) *big.Int {
+	for a := big.NewInt(1); a.Cmp(M) < 0; a.Add(a, big.NewInt(1)) {
+		fmt.Printf("Testing %v (M = %v)...\n", a, M)
+		if isWitness := isAKSWitness(n, r, a); isWitness {
+			return a
+		}
 	}
-	fmt.Printf("a %s an AKS witness for n\n", isWitnessStr)
+	return nil
+}
+
+func main() {
+	n := big.NewInt(46633)
+	r := big.NewInt(262)
+	M := big.NewInt(257)
+	fmt.Printf("n = %v, r = %v, M = %v\n", n, r, M)
+	a := getFirstAKSWitness(n, r, M)
+	if a != nil {
+		fmt.Printf("n is composite with AKS witness %v\n", a)
+	} else {
+		fmt.Printf("n is prime\n")
+	}
 }
