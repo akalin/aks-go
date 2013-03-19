8e4e70653f9d1a3a6ec5e893bd9afa666c451bc5
Frederick Akalin <akalin@gmail.com>
2013-03-19 01:48:11 -0700

    Implement and use concurrent witness-finding

--- aks.go
+++ aks.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 import "math/big"
+import "runtime"
 
 // Returns whether (X + a)^n = X^n + a mod (n, X^r - 1).
 func isAKSWitness(n, r, a *big.Int) bool {
@@ -33,12 +34,79 @@ func getFirstAKSWitness(n, r, M *big.Int) *big.Int {
 	return nil
 }
 
+// Holds the result of an AKS witness test.
+type witnessResult struct {
+	a         *big.Int
+	isWitness bool
+}
+
+// Tests all numbers received on numberCh if they are witnesses of n
+// with parameter r. Sends the results to resultCh.
+func testAKSWitnesses(
+	n, r *big.Int,
+	numberCh chan *big.Int,
+	resultCh chan witnessResult) {
+	for a := range numberCh {
+		fmt.Printf("Testing %v...\n", a)
+		isWitness := isAKSWitness(n, r, a)
+		fmt.Printf("Finished testing %v (isWitness=%t)\n",
+			a, isWitness)
+		resultCh <- witnessResult{a, isWitness}
+	}
+}
+
+// Returns an AKS witness of n with the parameters r and M, or nil if
+// there isn't one. Tests up to maxOutstanding numbers at once.
+func getAKSWitness(n, r, M *big.Int, maxOutstanding int) *big.Int {
+	numberCh := make(chan *big.Int, maxOutstanding)
+	defer close(numberCh)
+	resultCh := make(chan witnessResult, maxOutstanding)
+	for i := 0; i < maxOutstanding; i++ {
+		go testAKSWitnesses(n, r, numberCh, resultCh)
+	}
+
+	// Send off all numbers for testing, draining any results that
+	// come in while we're doing so.
+	tested := big.NewInt(1)
+	for i := big.NewInt(1); i.Cmp(M) < 0; {
+		select {
+		case result := <-resultCh:
+			tested.Add(tested, big.NewInt(1))
+			fmt.Printf("%v isWitness=%t\n",
+				result.a, result.isWitness)
+			if result.isWitness {
+				return result.a
+			}
+		default:
+			var a big.Int
+			a.Set(i)
+			numberCh <- &a
+			i.Add(i, big.NewInt(1))
+		}
+	}
+
+	// Drain any remaining results.
+	for tested.Cmp(M) < 0 {
+		result := <-resultCh
+		tested.Add(tested, big.NewInt(1))
+		fmt.Printf("%v isWitness=%t\n", result.a, result.isWitness)
+		if result.isWitness {
+			return result.a
+		}
+	}
+
+	return nil
+}
+
 func main() {
+	numCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(numCPU)
+
 	n := big.NewInt(46633)
 	r := big.NewInt(262)
 	M := big.NewInt(257)
 	fmt.Printf("n = %v, r = %v, M = %v\n", n, r, M)
-	a := getFirstAKSWitness(n, r, M)
+	a := getAKSWitness(n, r, M, numCPU)
 	if a != nil {
 		fmt.Printf("n is composite with AKS witness %v\n", a)
 	} else {
