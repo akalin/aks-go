37e28e116d7e5325d61ecea8f3df78e142fba822
Frederick Akalin <akalin@gmail.com>
2013-04-29 12:54:48 +0800

    Preallocate space in NewBigIntPoly and carefully use it

--- bigintpoly.go
+++ bigintpoly.go
@@ -18,7 +18,20 @@ type BigIntPoly struct {
 // Builds a new BigIntPoly representing the zero polynomial
 // mod (N, X^R - 1). R must fit into an int.
 func NewBigIntPoly(N, R big.Int) *BigIntPoly {
-	return &BigIntPoly{make([]big.Int, int(R.Int64()))}
+	rInt := int(R.Int64())
+	p := BigIntPoly{make([]big.Int, rInt)}
+
+	// Pre-allocate space for each coefficient. In order to take
+	// advantage of this, we must not assign to entries of
+	// p.coeffs directly but instead use big.Int.Set().
+	var nSq big.Int
+	nSq.Mul(&N, &N)
+	for i := 0; i < rInt; i++ {
+		p.coeffs[i].Set(&nSq)
+		p.coeffs[i].Set(&big.Int{})
+	}
+
+	return &p
 }
 
 // Sets p to X^k + a mod (N, X^R - 1).
@@ -26,11 +39,11 @@ func (p *BigIntPoly) Set(a, k, N big.Int) {
 	R := len(p.coeffs)
 	p.coeffs[0].Mod(&a, &N)
 	for i := 1; i < R; i++ {
-		p.coeffs[i] = big.Int{}
+		p.coeffs[i].Set(&big.Int{})
 	}
 	var i big.Int
 	i.Mod(&k, big.NewInt(int64(R)))
-	p.coeffs[int(i.Int64())] = *big.NewInt(1)
+	p.coeffs[int(i.Int64())].Set(big.NewInt(1))
 }
 
 // Returns whether p has the same coefficients as q.
@@ -49,7 +62,7 @@ func (p *BigIntPoly) Eq(q *BigIntPoly) bool {
 func (p *BigIntPoly) mul(q *BigIntPoly, N big.Int, tmp *BigIntPoly) {
 	R := len(tmp.coeffs)
 	for i := 0; i < R; i++ {
-		tmp.coeffs[i] = big.Int{}
+		tmp.coeffs[i].Set(&big.Int{})
 	}
 
 	for i := 0; i < R; i++ {
